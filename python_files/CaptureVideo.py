f2333ed04dc2a8d8e2a92f04f6c94b689ce8ac65
[Vishal Kumar] <[vishalkumarvkb1909@gmail.com]>
2023-05-19 11:26:13 +0530

    commit by vkb

--- python_files/CaptureVideo.py
+++ python_files/CaptureVideo.py
@@ -1,17 +1,35 @@
 import cv2
+import face_recognition as fr
 
 def captureFace():
     vc=cv2.VideoCapture(0)
+
+    processFrame=True
 
     while True:
 
         ret,frame=vc.read()
 
         if ret:
-            cv2.imshow("video",frame)
+            
+            if processFrame:
+
+                small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+
+                rgb_small_frame = small_frame[:, :, ::-1]
+        
+                face_locations = fr.face_locations(rgb_small_frame)
+                face_encodings = fr.face_encodings(rgb_small_frame, face_locations)
+
+                return face_encodings
+        
+        else:
+            print("camera not found")
 
         if cv2.waitKey(1) & 0xFF ==ord('q'):
             break
 
     vc.release()
     cv2.destroyAllWindows
+
+captureFace()
--- python_files/app.py
+++ python_files/app.py
@@ -1,6 +1,5 @@
 from flask import Flask,request
 from flask_mysqldb import MySQL
-import CaptureVideo as cv
 
 app=Flask(__name__)
 
@@ -11,12 +10,39 @@ app.config['MYSQL_DB']='test'
 
 mysql=MySQL(app)
 
+'''
 @app.route("/video")
 def func():
-    #cf=cv.captureFace()
+    fid=cv.captureFace()
     cur=mysql.connection.cursor()
-    cur.execute("CREATE table vishal(id int);")
+    cur.execute("CREATE table vishal(id int,faceid varchar);")
+    cur.execute("Insert into vishal(id,faceid) VALUES(1,"+str(fid)+")")
     return "created"
+'''
+
+@app.route("/create")
+def create():
+    cur=mysql.connection.cursor()
+    try:
+        cur.execute("CREATE table data(id int,name varchar,pword varchar)")
+        return "created"
+    except:
+        return "not created"
+
+@app.route("/update",methods=['POST'])
+def update():
+    data=request.get_json()
+    cur=mysql.connection.cursor()
+    cur.execute("")
+    return "updated"
+
+@app.route("/check",methods=['POST'])
+def check():
+    data=request.get_json()
+    if data:
+        cur=mysql.connection.cursor()
+        cur.execute("")
+    return "found"
 
 if __name__=="__main__":
     app.run()
